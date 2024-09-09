29db0b6058f5a4bd4edcecff38225460126e0afb
Mert Yılmaz <merttylmz_@outlook.com>
2024-09-09 18:18:53 +0300

    Initial Commit

---
+++ RickAndMorty-MVVMUITests/RickAndMorty_MVVMUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RickAndMorty_MVVMUITestsLaunchTests.swift
+//  RickAndMorty-MVVMUITests
+//
+//  Created by Mert Yılmaz on 9.09.2024.
+//
+
+import XCTest
+
+final class RickAndMorty_MVVMUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
